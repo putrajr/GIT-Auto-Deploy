bebfdd9dbcf786638447a5349719aaa754ef52a6
PUTRA <bayuputra.mobile@gmail.com>
2016-11-20 18:42:05 +0700

    init and final

---
+++ web-hook.php
@@ -0,0 +1,19 @@
+<?php
+set_include_path('/path/to/phpseclib-1.0.5/phpseclib/');
+
+include('phpseclib-1.0.5/phpseclib/Net/SSH2.php');
+//include('phpseclib-1.0.5/phpseclib/Crypt/RSA.php');
+
+//$ssh = new Net_SSH2('ip','port');
+$ssh = new Net_SSH2('xxx.xxx.xxx.xxx','22');
+//if (!$ssh->login('user root', 'password user root')) {
+//for using ssh/rsakey with/without password tutorial can see at:
+//1. ssh/rsa without password : http://phpseclib.sourceforge.net/ssh/auth.html#rsakey
+//2. ssh/rsa with password : http://phpseclib.sourceforge.net/ssh/auth.html#encrsakey
+if (!$ssh->login('root', 'xxxxxxx')) {
+    exit('Login Failed');
+}
+//$res = $ssh->exec('cd /var/www/my-repositories && git pull');
+$res = $ssh->exec('cd /path/to/repositories/cloned && git pull');
+echo $res;
+?>
