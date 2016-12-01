f7761491f11b977198b743acee7892221461e354
PUTRA <bayuputra.mobile@gmail.com>
2016-12-01 22:51:43 +0700

    no message

--- web-hook.php
+++ web-hook.php
@@ -1,22 +1,22 @@
 <?php
-$project_dir = "";				// "/var/www/html/deploy"
-$server_seperator="/";			// "/" in linux and unix, "\\" in windows
+$project_dir = "";						// "/var/www/html/deploy"
+$server_seperator="/";					// "/" in linux and unix, "\\" in windows
 
-$repository_url_ssh = "";		// "https://bitbucket.org/putrajr"
-$repository_name = "";			// "git-auto-deploy"
-$branches = "";					// "master"
+$repository_username_url_ssh = "";		// "https://bitbucket.org/putrajr"
+$repository_name = "";					// "git-auto-deploy"
+$branches = "";							// "master"
 
-$root_user = "";				// "root"
-$rsa_file_path="";				// "/root/server/ssh/xyz.file"
-$root_or_rsa_password = "";		// "123456"
+$root_user = "";						// "root"
+$rsa_file_path="";						// "/root/server/ssh/xyz.file"
+$root_or_rsa_password = "";				// "123456"
 
-$ip_server= "";					// "192.168.1.6"
-$port_ssh_server = "22";		// default port ssh is "22"
+$ip_server= "";							// "192.168.1.6"
+$port_ssh_server = "22";				// default port ssh is "22"
 
-$type_auth =1;					/* type_auth:
-								1. password
-								2. rsa without password
-								3. rsa with password */
+$type_auth =1;							/* type_auth:
+										1. password
+										2. rsa without password
+										3. rsa with password */
 
 set_include_path(__DIR__.'phpseclib-1.0.5/phpseclib/');
 
@@ -44,7 +44,7 @@ if($server_seperator=="/"){
 if(is_dir($project_dir.$server_seperator.$repository_name)){
 	$res = $ssh->exec($cmd.$project_dir.$server_seperator.$repository_name.' && git pull');
 }else{
-	$res = $ssh->exec($cmd.$project_dir.' && git clone -b '.$branch.' '.$repository_url_ssh.'/'.$repository_name);
+	$res = $ssh->exec($cmd.$project_dir.' && git clone -b '.$branch.' '.$repository_username_url_ssh.'/'.$repository_name);
 }
 echo $res;
 ?>
